7fc09f0d5a0e1369802efe7a1450763fe4f12c19
57459N <stasun008@gmail.com>
2024-07-12 00:17:20 +0300

    config

---
+++ config.py
@@ -0,0 +1,15 @@
+BOT_API_TOKEN = ""
+
+LINK_TO_WEBSITE = "https://link2website.example"
+LINK_TO_SUBSCRIBE = "https://link2sub.example"
+
+SUPPORT_USERNAME = "S7459N"
+LINK_TO_SUPPORT = f"https://t.me/{SUPPORT_USERNAME}"
+
+WALLET_EXPIRE_SECONDS = 10
+
+SERVER_HOST_IP = '127.0.0.1'
+SERVER_HOST_PORT = 8887
+
+SUB_COST = 50
+
