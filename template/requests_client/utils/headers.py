ff0e83d2c920292559c7ffddb7b7e278ccda3cb0
57459N <stasun008@gmail.com>
2024-08-07 00:02:47 +0300

    headders update

--- template/requests_client/utils/headers.py
+++ template/requests_client/utils/headers.py
@@ -1,5 +1,5 @@
 DEFAULT_HEADERS = {
-    'User-Agent': 'tls-client/0.2.2', 
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
     'Accept-Encoding': 'gzip, deflate', 
     'Accept': '*/*', 
     'Connection': 'keep-alive', 
