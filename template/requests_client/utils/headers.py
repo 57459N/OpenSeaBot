defc43eec44387cf9b22668a5fcadf44e42f0a83
57459N <stasun008@gmail.com>
2024-08-07 13:12:07 +0300

    headers update

--- template/requests_client/utils/headers.py
+++ template/requests_client/utils/headers.py
@@ -1,9 +1,7 @@
-DEFAULT_HEADERS = {'User-Agent': 'tls-client/1.0.1', 'Accept-Encoding': 'gzip, deflate, br', 'Accept': '*/*',
-                   'Connection': 'keep-alive'}
-# {
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-#     'Accept-Encoding': 'gzip, deflate',
-#     'Accept': '*/*',
-#     'Connection': 'keep-alive',
-#     'Content-Type': 'application/json'
-# }
+DEFAULT_HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept': '*/*',
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/json'
+}
