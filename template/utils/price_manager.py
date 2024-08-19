4852a705953064452a3224a48735b618478bf8fd
57459N <stasun008@gmail.com>
2024-08-19 22:48:11 +0300

    empty sessions url in price_manager to prevent requests collision

--- template/utils/price_manager.py
+++ template/utils/price_manager.py
@@ -3,6 +3,8 @@ import sys
 import aiohttp
 import loguru
 from typing import Union, Dict, Any
+import threading
+
 
 sys.path.append(str(pathlib.Path(__file__).parent.parent.parent.parent))
 import config
@@ -10,20 +12,23 @@ import config
 
 class PriceRequests:
     def __init__(self):
+        self.lock = threading.Lock()
         self.server_url = f'http://{config.SERVER_HOST_IP}:{config.SERVER_HOST_PORT}/price_parser'
 
     async def get_items_values(self, *slugs: str) -> Union[Dict[str, Any], float, str, None]:
-        async with aiohttp.ClientSession() as s:
-            async with s.post(f'{self.server_url}/get_prices?token={config.BOT_API_TOKEN}', json=slugs) as r:
-                try:
-                    return await r.json()
-                except Exception as e:
-                    loguru.logger.error(f'Error while requesting items prices from server\n{e}')
-                    return {}
+        with self.lock:
+            async with aiohttp.ClientSession() as s:
+                async with s.post(f'{self.server_url}/get_prices?token={config.BOT_API_TOKEN}', json=slugs) as r:
+                    try:
+                        return await r.json()
+                    except Exception as e:
+                        loguru.logger.error(f'Error while requesting items prices from server\n{e}')
+                        return {}
 
     async def submit_items(self, *slugs: str) -> None:
-        async with aiohttp.ClientSession() as s:
-            await s.post(f'{self.server_url}/add_collections?token={config.BOT_API_TOKEN}', json=slugs)
+        with self.lock:
+            async with aiohttp.ClientSession() as s:
+                await s.post(f'{self.server_url}/add_collections?token={config.BOT_API_TOKEN}', json=slugs)
 
 
 price_requests = PriceRequests()
