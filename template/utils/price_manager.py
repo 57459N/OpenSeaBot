51d9947b5eb9b5e6700b482a755d2d2369cb7d9c
57459N <stasun008@gmail.com>
2024-08-19 23:02:55 +0300

    empty sessions url in price_manager to prevent requests collision

--- template/utils/price_manager.py
+++ template/utils/price_manager.py
@@ -5,7 +5,6 @@ import loguru
 from typing import Union, Dict, Any
 import threading
 
-
 sys.path.append(str(pathlib.Path(__file__).parent.parent.parent.parent))
 import config
 
@@ -17,7 +16,7 @@ class PriceRequests:
 
     async def get_items_values(self, *slugs: str) -> Union[Dict[str, Any], float, str, None]:
         with self.lock:
-            async with aiohttp.ClientSession() as s:
+            async with aiohttp.ClientSession(trust_env=True) as s:
                 async with s.post(f'{self.server_url}/get_prices?token={config.BOT_API_TOKEN}', json=slugs) as r:
                     try:
                         return await r.json()
@@ -27,7 +26,7 @@ class PriceRequests:
 
     async def submit_items(self, *slugs: str) -> None:
         with self.lock:
-            async with aiohttp.ClientSession() as s:
+            async with aiohttp.ClientSession(trust_env=True) as s:
                 await s.post(f'{self.server_url}/add_collections?token={config.BOT_API_TOKEN}', json=slugs)
 
 
